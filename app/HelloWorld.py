08f3b2e0a6b38a0bda41d318225e93300718b11a
AlejandroCorona02 <alex124david123@gmail.com>
2023-12-13 08:14:33 -0600

    Adicion de comentarios

--- app/HelloWorld.py
+++ app/HelloWorld.py
@@ -4,6 +4,9 @@ app = Flask(__name__) #Aqui se crea una instancia que sea igual a la variable ap
 #Con el de arriba indicamos que este archivo sera nuestra aplicacion, y todos los recursos los buscara en este programa, por asi
 #decirle le indicamos que este es nuestro main, por ende aqui deberia de estar cualquier cosa usada en el programa 
 
-@app.route('/') #Aqui se crea una ruta,lo que hay entre parentesis indica que esta es la ruta principal y debe estar asociado a una funcion que sera un lista
+@app.route('/hello') #Aqui se crea una ruta,lo que hay entre parentesis indica que esta es la ruta principal, nos arrojara un url que tendra
+#numeros hasta antes del slash(/), despues lo que escribamos sera lo que debemos poner para ver la funcion asociada y debe estar asociado a 
+#una funcion, asi cada que se solicite el link arroje algo de regreso en este caso solo sera un texto 
+
 def hello():
     return 'Hola Mundo'
