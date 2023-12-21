d2906b2101aa3b616e1e1174bd7fc769d27783e6
AlejandroCorona02 <alex124david123@gmail.com>
2023-12-21 16:55:44 -0600

    Enlaces-Variables
    Se termino con el tema de variables y enlaces, aqui solo es el proyecto avanzado y haciendo referencia a varios archivos de ese tipo

--- app/Rutas.py
+++ app/Rutas.py
@@ -13,8 +13,16 @@ def today (Date):
 from datetime import datetime
 
 @app.route('/')
-@app.route('/Index')
 def Index():
+    #A continuacion se colocan los print para las url para hacer que el programa 
+    #los cree y no tengamos la necesidad de cambiar todo de manera manual, asi
+    #Solo escribimos lo siguiente y sera todo
+    print(url_for('Index'))
+    print(url_for('Prueba', Prueba = 'Si'))
+    print(url_for('Contenido',data = 'My_data'))
+    print(url_for('code', code = 'print("Hola")'))
+    
+    
     name = 'David' 
     Friends = ['Alan','Jaime','Diego','Luis','Juan','Monse','Camis']
     Date = datetime.now()
@@ -27,12 +35,32 @@ def Index():
 @app.route('/')
 @app.route('/Prueba')
 def Prueba():
-    Pruebas = 'Si'
+    Prueba = 'Si'
     return render_template('Prueba_1.html', 
-                           Pruebas = Pruebas)
+                           Prueba = Prueba)
 
-@app.route('/')
-def General():
-    print(url_for('Prueba',Pruebas = 'Si'))
-    print(url_for('Index'))
-    
+
+#Aqui comenzaremos a ver la parte de variables pero aplicadas a 
+#los html y no aqui en python como usualmente los estamos viendo
+@app.route('/Contenido')
+@app.route('/Contenido/<Name>')
+@app.route('/Contenido/<Name>/<int:Age>')
+@app.route('/Contenido/<Name>/<int:Age>/<email>')
+def Contenido(Name = 'Alex' , Age = '21', email = 'Alvyajalo@ajskjsa'):
+    #A esto se le llama estructura de datos
+    My_data = { #Este comenta el curso es un diccionario una especie de 
+        #almacenamiento de variables que aqui se obtienen y son enviadas
+        #a los html que lo requieran
+        'Name' : Name, 
+        'Age'  : Age,
+        'email': email
+    }
+    return render_template('Variables.html', data = My_data)#Aqui por lo
+#que entendi es que es una especie de carpeta donde almacenamos todas
+#nuestras variables, para asi solo llamar a la carpeta y no sera 
+#necesario hacer toda la declaracion de variables en algun otro lado
+
+from markupsafe import escape
+@app.route('/code/<path:code>')
+def code(code):
+    return render_template('Prueba_2.html')
